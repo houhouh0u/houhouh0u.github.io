99e06b0667c2eaf5a9921404abe9b994364ea7be
houhouh0u <houjiaqi@bupt.edu.cn>
2023-05-03 18:31:51 +0800

    all done

--- js/new/scrollchange.js
+++
@@ -1,8 +0,0 @@
-if ($("button") == null && $("#canvas2").attr("class") == "canvasU") {
-  var button = $("<button>");
-  button.attr("class", "load");
-  $("body").append(button);
-}
-$(".load").click(function () {
-  window.location.href = "30000.html";
-});
